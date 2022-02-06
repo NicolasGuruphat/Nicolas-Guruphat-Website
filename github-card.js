63cb9193a72ce21577caac861c4ebe15efd8986d
NicolasGuruphat <nicolas.guruphat@gmail.com>
2022-02-06 03:31:13 +0100

    ajout CSS + impact du CSS sur les repo-card

--- github-card.js
+++ github-card.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', async function () {
   const themes = {
     'light-default': {
       background: '#e8d6cb',
-      borderColor: '#e1e4e8',
+      borderColor: '#e8d6cb',
       color: '#7c3030',
       linkColor: '#0366d6',
     },
@@ -39,7 +39,8 @@ window.addEventListener('DOMContentLoaded', async function () {
     });
 
     el.innerHTML = `
-      <div style="font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji; border: 1px solid ${theme.borderColor}; border-radius: 6px; background: ${theme.background}; padding: 16px; font-size: 14px; line-height: 1.5; color: #24292e;">
+      
+      <div class="repo-card>
         <div style="display: flex; align-items: center;">
           <svg style="fill: ${theme.color}; margin-right: 8px;" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path></svg>
           <span style="font-weight: 600; color: ${theme.linkColor};">
